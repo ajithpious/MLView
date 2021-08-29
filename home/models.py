55a799a4ca9efd08afbae5cae3318d6636363780
AJITH <ajithpiousolive@gmail.com>
2021-08-29 17:41:43 +0530

    postgress changes

--- home/models.py
+++ home/models.py
@@ -5,6 +5,6 @@ class user(models.Model):
     username=models.CharField(max_length=20,primary_key=True)
     password=models.CharField(max_length=20)
     class Meta:
-        db_table="user"
+        db_table="my_data"
 
 
